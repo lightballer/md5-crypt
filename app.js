2d6374cc9d1d1f04876b9e1199562dbef3a34c7c
Explodeus <volodya.sokha@gmail.com>
2020-01-03 19:11:08 +0200

    Cryptor ready to use

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 var md5 = require('md5');
 const readline = require('readline');
+var ReverseMd5 = require('reverse-md5');
 
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
+});
+
+var reverseMd5 = ReverseMd5({
+	lettersUpper: false,
+	lettersLower: true,
+	numbers: true,
+	special: false,
+	whitespace: true,
+	maxLen: 12
 });
 
 rl.question(
@@ -12,24 +22,18 @@ rl.question(
 Your mode: `, (chooseMode) => {
     if (chooseMode == '1') {
         console.log('You choosen Encrypt mode');
-        encrypt();
+        rl.question(`Enter your message to encrypt: `, (encryptMessage) => {
+            console.log('Your encrypted message: ', md5(encryptMessage));
+            rl.close();
+        });
     } else if (chooseMode == '2') {
         console.log('You choosen Decrypt mode');
-        decrypt();
+        rl.question(`Enter message to decrypt: `, (decryptMessage) =>{
+            console.log('Your message decrypted: ', reverseMd5(decryptMessage).str);
+            rl.close();
+        });
     } else {
         console.log('Incorrect input');
+        rl.close();
     }
-
-    rl.close();
 });
-
-function encrypt() {
-    rl.question('Enter your message to encrypt: ', (encryptMessage) => {
-        console.log('Your encrypted message: ', md5(encryptMessage));
-    });
-}
-function decrypt(){
-    rl.question('Enter message to decrypt: ', (decryptMessage) =>{
-        console.log('Your message decrypted: ', decrypt(decryptMessage));
-    });
-}
