f803f4158ef10ed9b55f510adbec2f668ae90f3a
yehorbk <e.bublyk1010@gmail.com>
2020-01-04 20:03:45 +0200

    code refactored

--- app.js
+++ app.js
@@ -1,39 +1,39 @@
-var md5 = require('md5');
 const readline = require('readline');
-var ReverseMd5 = require('reverse-md5');
+const md5 = require('md5');
+const ReverseMd5 = require('reverse-md5');
 
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-var reverseMd5 = ReverseMd5({
-	lettersUpper: false,
-	lettersLower: true,
-	numbers: true,
-	special: false,
-	whitespace: true,
-	maxLen: 12
+const reverseMd5 = ReverseMd5({
+    lettersUpper: false,
+    lettersLower: true,
+    numbers: true,
+    special: false,
+    whitespace: true,
+    maxLen: 12
 });
 
-rl.question(
-    `Please, choose mode
-1 - Encrypt, 2 - Decrypt
-Your mode: `, (chooseMode) => {
-    if (chooseMode == '1') {
-        console.log('You choosen Encrypt mode');
-        rl.question(`Enter your message to encrypt: `, (encryptMessage) => {
-            console.log('Your encrypted message: ', md5(encryptMessage));
-            rl.close();
-        });
-    } else if (chooseMode == '2') {
-        console.log('You choosen Decrypt mode');
-        rl.question(`Enter message to decrypt: `, (decryptMessage) =>{
-            console.log('Your message decrypted: ', reverseMd5(decryptMessage).str);
-            rl.close();
-        });
-    } else {
-        console.log('Incorrect input');
-        rl.close();
+const encryptUserMessage = data => 
+    console.log('Your encrypted message: ', md5(data));
+const decryptUserMessage = data =>
+    console.log('Your message decrypted: ', reverseMd5(data).str);
+
+const chooseModeCallback = mode => {
+    switch (mode) {
+        case '1':
+            console.log('You choosen Encrypt mode');
+            rl.question(`Enter your message to encrypt: `, encryptUserMessage);
+            break;
+        case '2':
+            console.log('You choosen Decrypt mode');
+            rl.question(`Enter message to decrypt: `, decryptUserMessage);
+            break;
+        default:
+            console.log('Incorrect input');
     }
-});
+}
+
+rl.question('Please, choose mode\n1 - Encrypt, 2 - Decrypt\nYour mode: ', chooseModeCallback);
