8867f742fa957371af1bbbb008998b5cf632f130
yehorbk <e.bublyk1010@gmail.com>
2020-01-04 22:02:13 +0200

    library moved to another directory

--- crypt.js
+++ crypt.js
@@ -1,25 +1,12 @@
 const readline = require('readline');
-const md5 = require('md5');
-const ReverseMd5 = require('reverse-md5');
+const { encrypt, decrypt } = require('./lib/crypt');
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
-const reverseMd5 = ReverseMd5({
-  lettersUpper: false,
-  lettersLower: true,
-  numbers: true,
-  special: false,
-  whitespace: true,
-  maxLen: 12
-});
-
 const commandLineKeys = ['--enc', '--dec'];
-
-const encrypt = data => md5(data);
-const decrypt = data => reverseMd5(data).str;
 
 const encryptUserMessage = message =>
   console.log('Your encrypted message: ', encrypt(message));
