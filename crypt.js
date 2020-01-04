f181c2621e13d9ff97f77f005a7d5be968c91b56
yehorbk <e.bublyk1010@gmail.com>
2020-01-04 21:56:05 +0200

    linter added and configured, crypt.js refactored

--- crypt.js
+++ crypt.js
@@ -3,17 +3,17 @@ const md5 = require('md5');
 const ReverseMd5 = require('reverse-md5');
 
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout
 });
 
 const reverseMd5 = ReverseMd5({
-    lettersUpper: false,
-    lettersLower: true,
-    numbers: true,
-    special: false,
-    whitespace: true,
-    maxLen: 12
+  lettersUpper: false,
+  lettersLower: true,
+  numbers: true,
+  special: false,
+  whitespace: true,
+  maxLen: 12
 });
 
 const commandLineKeys = ['--enc', '--dec'];
@@ -21,46 +21,47 @@ const commandLineKeys = ['--enc', '--dec'];
 const encrypt = data => md5(data);
 const decrypt = data => reverseMd5(data).str;
 
-const encryptUserMessage = message => 
-    console.log('Your encrypted message: ', encrypt(message));
+const encryptUserMessage = message =>
+  console.log('Your encrypted message: ', encrypt(message));
 const decryptUserMessage = message =>
-    console.log('Your message decrypted: ', decrypt(message));
+  console.log('Your message decrypted: ', decrypt(message));
 
 const chooseModeCallback = mode => {
-    switch (mode) {
-        case '1':
-            console.log('You choosen Encrypt mode');
-            rl.question(`Enter your message to encrypt: `, encryptUserMessage);
-            break;
-        case '2':
-            console.log('You choosen Decrypt mode');
-            rl.question(`Enter message to decrypt: `, decryptUserMessage);
-            break;
-        default:
-            console.log('Incorrect input');
-    }
-}
+  switch (mode) {
+  case '1':
+    console.log('You choosen Encrypt mode');
+    rl.question('Enter your message to encrypt: ', encryptUserMessage);
+    break;
+  case '2':
+    console.log('You choosen Decrypt mode');
+    rl.question('Enter message to decrypt: ', decryptUserMessage);
+    break;
+  default:
+    console.log('Incorrect input');
+  }
+};
 
 
 // Entry Point //
 
 ((key, data) => {
-    if (key && data) {
-        if (commandLineKeys.includes(key)) {
-            switch(key) {
-                case '--enc':
-                    console.log(encrypt(data));
-                    break;
-                case '--dec':
-                    console.log(decrypt(data));
-                    break;
-            }
-            rl.close();
-        } else {
-            console.log('Unknown command!');
-        }
+  if (key && data) {
+    if (commandLineKeys.includes(key)) {
+      switch (key) {
+      case '--enc':
+        console.log(encrypt(data));
+        break;
+      case '--dec':
+        console.log(decrypt(data));
+        break;
+      }
+      rl.close();
     } else {
-        rl.question('Please, choose mode\n1 - Encrypt, 2 - Decrypt\nYour mode: ', chooseModeCallback);
+      console.log('Unknown command!');
     }
+  } else {
+    const entryMessage = 'Please, choose mode\n' +
+        '1 - Encrypt, 2 - Decrypt\nYour mode: ';
+    rl.question(entryMessage, chooseModeCallback);
+  }
 })(process.argv[2], process.argv[3]);
-
